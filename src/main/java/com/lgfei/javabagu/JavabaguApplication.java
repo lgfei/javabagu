f07acb83626fd3071493491cd164787d18f7124d
long.gf <long.gf@topsports.com.cn>
2023-10-31 15:56:55 +0800

    init by https://start.spring.io/

---
+++ src/main/java/com/lgfei/javabagu/JavabaguApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.javabagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabaguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgfei/javabagu/JavabaguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.javabagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
